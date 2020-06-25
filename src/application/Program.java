a696d86cabf7ee31a022a6dc3abd5e5bcefe289b
Anilton Viganigo Junior <viganigojr@gmail.com>
2020-06-24 22:13:03 -0300

    Declaração de uma classe anônima dentro da classe Program.

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,13 +12,22 @@ public class Program {
 	public static void main(String[] args) {
 
 		List<Product> list = new ArrayList<>();
-		
+
 		list.add(new Product("Televisão", 900.00));
 		list.add(new Product("Fogão", 700.00));
 		list.add(new Product("Notebook", 2500.00));
-		
-		list.sort(new MyComparator());
-		
+
+		Comparator<Product> comp = new Comparator<Product>() {
+
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().compareTo(p2.getName().toUpperCase());
+			}
+
+		};
+
+		list.sort(comp);
+
 		for (Product p : list) {
 			System.out.println(p);
 		}
